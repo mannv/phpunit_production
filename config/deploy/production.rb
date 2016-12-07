1ebb957f75fc6923f0598d4083e52cdebf1f2d5a
man.nv <man.nv@kayac.vn>
2016-12-07 17:47:04 +0700

    first commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -64,23 +64,23 @@ set :deploy_to, '/home/ubuntu/capistrano/prod'
 set :branch, 'master'
 # set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader'
 
-# server '35.163.165.15',
-#   user: 'ubuntu',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'ubuntu', # overrides user setting above
-#     keys: %w(time_management_system.pem),
-#     forward_agent: false
-#   }
-
-server '35.165.3.50',
+server '35.163.165.15',
   user: 'ubuntu',
   roles: %w{web app},
   ssh_options: {
     user: 'ubuntu', # overrides user setting above
-    keys: %w(new_key_pair.pem),
+    keys: %w(time_management_system.pem),
     forward_agent: false
-  }  
+  }
+
+# server '35.165.3.50',
+#   user: 'ubuntu',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'ubuntu', # overrides user setting above
+#     keys: %w(new_key_pair.pem),
+#     forward_agent: false
+#   }  
 
 namespace :deploy do
     desc 'Print The Server Name'
